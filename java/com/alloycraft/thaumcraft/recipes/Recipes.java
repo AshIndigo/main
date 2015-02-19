23a62b4986daa6eb2627ccd9117a3ffafbd9ae76
Ash <firedragonman958@gmail.com>
2015-02-19 13:48:37 +0100

    Almost got recipes

--- java/com/alloycraft/thaumcraft/recipes/Recipes.java
+++ java/com/alloycraft/thaumcraft/recipes/Recipes.java
@@ -1,13 +1,21 @@
 package com.alloycraft.thaumcraft.recipes;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import com.alloycraft.exxo.Alloycraft;
 import com.alloycraft.exxo.recipes.*;
 
 public class Recipes {
 
 	public static AlloyFurnaceRecipes AlloyFurnaceRecipes;
-	public static void loadRecipes() {
-		AlloyFurnaceRecipes.getOutput(Item item, Item item2);{
+	public static ItemStack loadRecipes(Item item, Item item2) {
+		AlloyFurnaceRecipes.getOutput(item, item2);{
+			if (item == Items.gold_ingot && item2 == Items.gold_ingot || item == Items.gold_ingot && item2 == Items.gold_ingot) {
+				return new ItemStack(Alloycraft.goldironingot, 2);
+			}
+			return null;
 			
 		}
 		
